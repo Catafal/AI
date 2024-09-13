57c4afe3743a91b951943acfd26622627c44f6b6
Catafal <jcatafal999@gmail.com>
2024-09-13 14:05:01 +0200

    Create group_26.r

---
+++ group_26.r
@@ -0,0 +1,13 @@
+# File:         group_26 
+# Description:  Naive demo-solution given at classroom session
+#               for Project 1, Artificial Intelligence 2019, UU
+# Author:       Fredrik Nilsson
+# Modified by:  Marcello Vendruscolo (2021)
+
+# Install the package
+# install.packages("DeliveryMan_1.1.0.tar.gz", repos = NULL, type="source")
+
+# Load the library
+library(DeliveryMan)
+
+
